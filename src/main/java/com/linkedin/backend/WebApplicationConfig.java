8d9779794fd3acaf6a8290d00a95abeedace16ec
Ilias Tsatiris <eliastsatiris@gmail.com>
2018-09-28 00:36:28 +0300

    Add HTTPS

--- src/main/java/com/linkedin/backend/WebApplicationConfig.java
+++ src/main/java/com/linkedin/backend/WebApplicationConfig.java
@@ -1,18 +1,10 @@
 package com.linkedin.backend;
 
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 // TODO: Enable when using Spring as the app server.
-//@Controller
+// @Controller
 class WebApplicationConfig {
 
     // Match everything without a suffix (so not a static resource)
