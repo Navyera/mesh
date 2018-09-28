7c0e658b709b58e8833214b650eac87123ba37fd
Ilias Tsatiris <eliastsatiris@gmail.com>
2018-09-28 18:38:06 +0300

    Change return type of search endpoint

---
+++ src/main/java/com/linkedin/backend/dto/SearchResultDTO.java
@@ -0,0 +1,39 @@
+package com.linkedin.backend.dto;
+
+import com.linkedin.backend.user.dao.AppUser;
+
+public class SearchResultDTO {
+    private Integer userID;
+    private String firstName;
+    private String lastName;
+
+    public SearchResultDTO(AppUser user) {
+        userID    = user.getId();
+        firstName = user.getFirstName();
+        lastName  = user.getLastName();
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- src/main/java/com/linkedin/backend/user/controllers/NetworkController.java
+++ src/main/java/com/linkedin/backend/user/controllers/NetworkController.java
@@ -1,9 +1,8 @@
 package com.linkedin.backend.user.controllers;
 
 import com.linkedin.backend.dto.ProfileDTO;
-import com.linkedin.backend.dto.UserDetailsDTO;
+import com.linkedin.backend.dto.SearchResultDTO;
 import com.linkedin.backend.user.AppUserService;
-import com.linkedin.backend.user.dao.AppUser;
 import com.linkedin.backend.utils.JSONReturn;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,18 +20,18 @@ public class NetworkController {
     }
 
     @PostMapping("/search")
-    public List<ProfileDTO> searchUsers(@Valid @RequestBody JSONReturn<String> searchTerms) {
+    public List<SearchResultDTO> searchUsers(@Valid @RequestBody JSONReturn<String> searchTerms) {
         String[] terms = searchTerms.getPayload().split(" ", -2);
 
         String firstTerm = terms[0].trim().toLowerCase();
 
-        String secondTerm = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+        String secondTerm = "________________________________";
         if (terms.length == 2)
             secondTerm = terms[1].trim().toLowerCase();
 
         return appUserService.getSearchResults(firstTerm, secondTerm)
                              .stream()
-                             .map(ProfileDTO::new)
+                             .map(SearchResultDTO::new)
                              .collect(Collectors.toList());
     }
 }
